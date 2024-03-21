073f6bd9799db96368cb6c14631eaabda0edbe26
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-03-20 19:17:00 -0600

    done!

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,7 @@
     <router-view />
   </main>
   <footer>
-    <span>©2024 AJ Vancattenburch | All Rights Reserved </span>
-    <div class="col-4 d-flex justify-content-end">
-      <span>Contact Website Creator:</span>
-      <a href="https://my3dlandingpage.web.app/" target="_blank">
-        <i class="mdi mdi-web" title="Creator's Portfolio Website">Website</i>
-      </a>
-      <a href="https://my3dlandingpage.web.app/" target="_blank">
-        <i class="mdi mdi-web" title="Creator's Portfolio Website">Website</i>
-      </a>
-      <a href="https://my3dlandingpage.web.app/" target="_blank">
-        <i class="mdi mdi-web" title="Creator's Portfolio Website">Website</i>
-      </a>
-    </div>
+    <PageFooter />
   </footer>
 </template>
 
@@ -26,6 +14,7 @@
 import { computed } from 'vue'
 import { AppState } from './AppState'
 import Navbar from './components/Navbar.vue'
+import PageFooter from './components/PageFooter.vue'
 
 export default {
   setup() {
@@ -33,7 +22,7 @@ export default {
       appState: computed(() => AppState)
     }
   },
-  components: { Navbar }
+  components: { Navbar, PageFooter }
 }
 </script>
 <style lang="scss">
@@ -42,24 +31,10 @@ export default {
 :root{
   --main-height: calc(100vh - 32px - 64px);
 }
-
 * {
   font-family: 'Futura' Condensed Medium;
   scroll-behavior: smooth;
   margin: 0;
   padding: 0;
-}
-
-footer {
-  display: flex;
-  justify-content: space-evenly;
-  width: 100%;
-  height: 35px;
-  color: var(--retro-cream);
-  font-weight: 500;
-  background-color: var(--retro-brown);
-  position: fixed;
-  bottom: 0;
-  z-index: 2;
 }
 </style>
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="col-12 d-flex justify-content-center align-items-center">
+    <div class="author">
+      <span>©</span>2024 All Rights Reserved | Developed by <a href="https://my3dlandingpage.web.app/" target="_blank" style="filter:brightness(2);">Anthony Vancattenburch</a>
+    </div>
+    <div class="contact-info">
+      <span>Contact: </span>
+      <a class="web-icon" href="https://my3dlandingpage.web.app/" target="_blank">
+        <i class="mdi mdi-web" title="Visit creator's Portfolio Website"></i>
+      </a>
+      <a class="linkedin-icon" href="https://linkedin.com/in/anthonyvancattenburch/" target="_blank">
+        <i class="mdi mdi-linkedin" title="Visit creator's LinkedIn Profile page"></i>
+      </a>
+      <a class="github-icon" href="https://github.com/AJVancattenburch" target="_blank">
+        <i class="mdi mdi-github" title="Visit creator's Github Profile page"></i>
+      </a>
+      <a class="twitter-icon" href="https://twitter.com/AVancattenburch" target="_blank">
+        <i class="mdi mdi-twitter" title="Visit creator's Twitter Profile page"></i>
+      </a>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+@import url('../assets/scss/_page_footer.scss');
+</style>
